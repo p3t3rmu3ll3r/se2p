9db79f564724a305af69c1f4901f925ff61b486b
martin <martin.slowikowski@haw-hamburg.de>
2012-11-08 13:05:23 +0100

    lc next try

--- HAL/LightController.cpp
+++ HAL/LightController.cpp
@@ -1,105 +1,121 @@
 /**
- * SE2 WiSe 2012
- * LightController for the traffic lights
- *
- * Offers functions for different states the festo
- * system can have. Running in a Thread. Status of
- * the traffic light is manipulated through public
- * accessible functions.
- *
- * \file LightController.cpp
- * \author Chris Addo
- *         Jens Eberwein
- *         Tristan Rudat
- *         Martin Slowikowski
- * \date 2012-10-29
- * \version 0.4
- *
- */
+* SE2 WiSe 2012
+* LightController for the traffic lights
+*
+* Offers functions for different states the festo
+* system can have. Running in a Thread. Status of
+* the traffic light is manipulated through public
+* accessible functions.
+*
+* \file LightController.cpp
+* \author Chris Addo
+* Jens Eberwein
+* Tristan Rudat
+* Martin Slowikowski
+* \date 2012-10-29
+* \version 0.4
+*
+*/
 
 #include "LightController.h"
-LightController *LightController::instance = 0;
 
-LightController *LightController::getInstance() {
-	if (instance == 0) {
-		instance = new LightController();
-	}
-	return instance;
-}
+LightController* LightController::instance = NULL;
+Mutex* LightController::lightInstanceMutex = new Mutex();
 
 LightController::LightController() {
-	aHal = ActorHAL::getInstance();
-	function = &LightController::off;
-	start(0);
-	hold();
+aHal = ActorHAL::getInstance();
+
+function = &LightController::off;
+start(0);
+hold();
 }
 
 LightController::~LightController() {
+if(instance != NULL){
+delete instance;
+instance = NULL;
+lightInstanceMutex->~Mutex();
+}
+}
+
+LightController* LightController::getInstance() {
+if (!instance) {
+lightInstanceMutex->lock();
+if (!instance) {
+instance = new LightController();
+#ifdef DEBUG_LIGHTCONTROLLER
+printf("Debug LightController: New LC instance created\n");
+#endif
+}
+lightInstanceMutex->unlock();
+}
+
+return instance;
 }
 
 void LightController::execute(void*) {
-	while (!isStopped()) {
-		(this->*function)();
-	}
+while (!isStopped()) {
+(this->*function)();
+}
 }
 
 void LightController::shutdown() {
 }
 
 void LightController::operatingNormal() {
-	lightsOff();
-	aHal->lightGreen(true);
+lightsOff();
+aHal->lightGreen(true);
 }
 
 void LightController::manualTurnover() {
-	lightsOff();
-	changeState(MANUAL_TURNOVER);
-	cont();
+lightsOff();
+function = &LightController::blinkYellow;
+cont();
 }
 
 void LightController::upcomingNotReceipted() {
-	lightsOff();
-	changeState(UPCOMING_NOT_RECEIPTED);
-	cont();
+lightsOff();
+function = &LightController::blinkRedSlow;
+cont();
 }
 
-void LightController::upcomingReceipted(){
-	lightsOff();
-	aHal->lightRed(true);
+void LightController::upcomingReceipted() {
+lightsOff();
+aHal->lightRed(true);
 }
 
-void LightController::goneUnreceipted(){
-	lightsOff();
-	changeState(GONE_UNRECEIPTED);
-	cont();
-}
-
-void LightController::lightsOff() {
-	aHal->lightsOff();
-	off();
-}
-
-void LightController::off() {
-	hold();
+void LightController::goneUnreceipted() {
+lightsOff();
+function = &LightController::blinkRedFast;
+cont();
 }
 
 void LightController::blinkYellow() {
-	aHal->lightYellow(true);
-	sleep(1);
-	aHal->lightYellow(false);
-	sleep(1);
+aHal->lightYellow(true);
+sleep(1);
+aHal->lightYellow(false);
+sleep(1);
 }
 
 void LightController::blinkRedFast() {
-	aHal->lightRed(true);
-	sleep(1);
-	aHal->lightRed(false);
-	sleep(1);
+aHal->lightRed(true);
+sleep(1);
+aHal->lightRed(false);
+sleep(1);
 }
 
 void LightController::blinkRedSlow() {
-	aHal->lightRed(true);
-	usleep(500000);
-	aHal->lightRed(false);
-	usleep(500000);
+aHal->lightRed(true);
+usleep(500000);
+aHal->lightRed(false);
+usleep(500000);
+}
+
+void LightController::lightsOff() {
+aHal->lightsOff();
+off();
+}
+
+void LightController::off() {
+hold();
 }
--- HAL/LightController.h
+++ HAL/LightController.h
@@ -1,32 +1,135 @@
+/**
+* SE2 WiSe 2012
+* LightController for the traffic lights
+*
+* Offers functions for different states the festo
+* system can have. Running in a Thread. Status of
+* the traffic light is manipulated through public
+* accessible functions.
+*
+* \file LightController.h
+* \author Chris Addo
+* Jens Eberwein
+* Tristan Rudat
+* Martin Slowikowski
+* \date 2012-10-29
+* \version 0.4
+*
+*/
+
 #ifndef LIGHTCONTROLLER_H_
 #define LIGHTCONTROLLER_H_
 
-#include <iostream>
-#include "HWaccess.h"
-#include "HAWThread.h"
+#define DEBUG_LIGHTCONTROLLER
+
 #include "ActorHAL.h"
+#include "HAWThread.h"
 
-class LightController: public thread::HAWThread {
+class LightController : public thread::HAWThread {
 public:
-	static LightController* getInstance();
-	void execute(void*);
-	void shutdown();
-	virtual ~LightController();
+/**
+* Standard destructor
+*/
+virtual ~LightController();
 
-	void operatingNormal();
-	void manualTurnover();
-	void upcomingNotReceipted();
-	void upcomingReceipted();
-	void goneUnreceipted();
-	void lightsOff();
+/**
+* Executes the chosen light function, initially starts with
+* all lighs off. function to start with configured in constructor
+*/
+virtual void execute(void* arg);
+
+/**
+* Empty shutdown() function
+*/
+virtual void shutdown();
+
+/**
+* Returns instance to work with LightController (Singleton)
+* \return LightController instance
+*/
+static LightController* getInstance();
+
+
+/**
+* Signals an upcoming error, not yet receipted.
+* Red light blinking fast with 1Hz
+*/
+void upcomingNotReceipted();
+
+/**
+* Signals an upcoming and receipted error.
+* Red light on
+*/
+void upcomingReceipted();
+
+/**
+* Signals an error gone but not receipted.
+* Red light blinking slow with 0,5Hz
+*/
+void goneUnreceipted();
+
+/**
+* Signals normal operating system
+* Green light on
+*/
+void operatingNormal();
+
+/**
+* Signals a workpiece with opening at top at the end
+* of conveyor 1.
+* Yellow light blinking
+*/
+void manualTurnover();
+
+/**
+* Switches all lights of and suspends the thread.
+*/
+void lightsOff();
+
+
 private:
-	void off();
-	LightController();
-	static LightController *instance;
-	void (LightController::*function)(void);
-	ActorHAL *aHal;
-	void blinkYellow();
-	void blinkRedFast();
-	void blinkRedSlow();
+/**
+* Private constructor for singleton. Starts and then
+* suspends the thread. Initial function is lightsOff();
+*/
+LightController();
+
+/**
+* HAL instance to get access to actors.
+*/
+ActorHAL* aHal;
+
+/**
+* Mutex for creation of the LightController instance
+*/
+static Mutex* lightInstanceMutex;
+
+/**
+* Instance for using LightController (Singleton)
+*/
+static LightController* instance;
+
+/**
+* Function-pointer for the execute function. Can be set by
+* the public methods to be executed in the thread.
+*/
+void (LightController::*function)(void);
+
+/**
+* Blink yellow
+*/
+void blinkYellow();
+
+/**
+* Blink red fast
+*/
+void blinkRedFast();
+
+/**
+* Blink red slow
+*/
+void blinkRedSlow();
+
 };
+
 #endif /* LIGHTCONTROLLER_H_ */
