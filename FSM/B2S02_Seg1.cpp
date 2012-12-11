2d9f0b925d27ca5b86ffc9db15a28108a08c32ff
chris <christopher.addo@haw-hamburg.de>
2012-12-11 16:39:53 +0100

    band2 changed

--- FSM/B2S02_Seg1.cpp
+++ FSM/B2S02_Seg1.cpp
@@ -46,31 +46,26 @@ void B2S02_Seg1::sbHeightcontrolOpen() {
 }
 
 void B2S02_Seg1::timerSeg1Min() {
-	if(controller->isFirstElementInSegment()) {
-		controller->setSegTimerMinCalled(true);
-	}
+	controller->setSegTimerMinCalled(true);
 }
 
 void B2S02_Seg1::timerSeg1Max() {
-	if(controller->isFirstElementInSegment()) {
+	int errorfsmChid = errfsm->getErrorFSMChid();
+	int errorfsmCoid;
+	int rc;
 
-		int errorfsmChid = errfsm->getErrorFSMChid();
-		int errorfsmCoid;
-		int rc;
+	if ((errorfsmCoid = ConnectAttach(0, 0, errorfsmChid, _NTO_SIDE_CHANNEL, 0)) == -1) {
+		printf("B2S02_Seg1: Error in ConnectAttach\n");
+	}
 
-		if ((errorfsmCoid = ConnectAttach(0, 0, errorfsmChid, _NTO_SIDE_CHANNEL, 0)) == -1) {
-			printf("B2S02_Seg1: Error in ConnectAttach\n");
-		}
+	//rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_CRITICAL_ERROR);
+	rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_ERROR);
+	if (rc < 0) {
+		printf("B2S02_Seg1: Error in MsgSendPulse");
+	}
 
-		//rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_CRITICAL_ERROR);
-		rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_ERROR);
-		if (rc < 0) {
-			printf("B2S02_Seg1: Error in MsgSendPulse");
-		}
-
-		if (ConnectDetach(errorfsmCoid) == -1) {
-			printf("B2S02_Seg1: Error in ConnectDetach\n");
-		}
+	if (ConnectDetach(errorfsmCoid) == -1) {
+		printf("B2S02_Seg1: Error in ConnectDetach\n");
 	}
 
 	puckHandler->removePuckFromBand(controller);
--- FSM/B2S04_Seg2.cpp
+++ FSM/B2S04_Seg2.cpp
@@ -49,31 +49,26 @@ void B2S04_Seg2::msMetalTrue() {
 }
 
 void B2S04_Seg2::timerSeg2Min() {
-	if(controller->isFirstElementInSegment()) {
-		controller->setSegTimerMinCalled(true);
-	}
+	controller->setSegTimerMinCalled(true);
 }
 
 void B2S04_Seg2::timerSeg2Max() {
-	if(controller->isFirstElementInSegment()) {
+	int errorfsmChid = errfsm->getErrorFSMChid();
+	int errorfsmCoid;
+	int rc;
 
-		int errorfsmChid = errfsm->getErrorFSMChid();
-		int errorfsmCoid;
-		int rc;
+	if ((errorfsmCoid = ConnectAttach(0, 0, errorfsmChid, _NTO_SIDE_CHANNEL, 0)) == -1) {
+		printf("B2S04_Seg2: Error in ConnectAttach\n");
+	}
 
-		if ((errorfsmCoid = ConnectAttach(0, 0, errorfsmChid, _NTO_SIDE_CHANNEL, 0)) == -1) {
-			printf("B2S04_Seg2: Error in ConnectAttach\n");
-		}
+	//rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_CRITICAL_ERROR);
+	rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_ERROR);
+	if (rc < 0) {
+		printf("B2S04_Seg2: Error in MsgSendPulse");
+	}
 
-		//rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_CRITICAL_ERROR);
-		rc = MsgSendPulse(errorfsmCoid, SIGEV_PULSE_PRIO_INHERIT, PULSE_FROM_PUCK, ERR_STATE_ERROR);
-		if (rc < 0) {
-			printf("B2S04_Seg2: Error in MsgSendPulse");
-		}
-
-		if (ConnectDetach(errorfsmCoid) == -1) {
-			printf("B2S04_Seg2: Error in ConnectDetach\n");
-		}
+	if (ConnectDetach(errorfsmCoid) == -1) {
+		printf("B2S04_Seg2: Error in ConnectDetach\n");
 	}
 
 	puckHandler->removePuckFromBand(controller);
